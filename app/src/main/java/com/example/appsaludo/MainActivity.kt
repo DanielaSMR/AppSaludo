9c1b73e5033aa4f8ce2403be6b5d5bdc2e0ab514
«DanielaSMR» <10828775@ieselgrao.org>
2024-10-16 12:10:47 +0200

    1.2

--- app/src/main/java/com/example/appsaludo/MainActivity.kt
+++ app/src/main/java/com/example/appsaludo/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.appsaludo
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val etName:AppCompatEditText = findViewById(R.id.etName)
+        val btnHello:AppCompatButton = findViewById(R.id.btnHello)
+        btnHello.setOnClickListener {
+                val name = etName.text.toString()
+                //Log.i("Test", "Boton Pulsado")
+                //Toast.makeText(this,"Hola $name!", Toast.LENGTH_LONG).show()
+            if(name.isNotEmpty()){
+                Snackbar.make(btnHello,"Hola $name!", Snackbar.LENGTH_SHORT)
+                    .setAnchorView(btnHello)
+                    .setAction("CLOSE"){finishAffinity()}
+                    .show()
+            }
+        }
     }
 }
